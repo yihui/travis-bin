64c650a3048eeff9823f7e6d7d34aa23ae14d721
Yihui Xie <xie@yihui.name>
2017-10-20 22:13:56 -0500

    use stack.pkg.yaml to build self-contained binaries

--- index.sh
+++ index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</title></head><body><ul>'
-for i in `ls`; do echo "<li><a href=\"$i\">$i</a></li>"; done
+for i in `ls dist`; do echo "<li><a href=\"$i\">$i</a></li>"; done
 echo '</ul></body></html>'
