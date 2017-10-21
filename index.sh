072ca6b69156c2a7e4d1fdbfb79f061cf6671d01
Yihui Xie <xie@yihui.name>
2017-10-20 21:47:43 -0500

    generate a homepage

---
+++ index.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</title></head><body><ul>'
+for i in `ls`; do echo "<li><a href=\"$i\">$i</a></li>"; done
+echo '</ul></body></html>'
