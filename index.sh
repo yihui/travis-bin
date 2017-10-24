4726c2fd04570ddd39d53fc22030da80920c741a
Yihui Xie <xie@yihui.name>
2017-10-23 22:37:22 -0500

    write out the time of the last update

--- index.sh
+++ index.sh
@@ -2,4 +2,6 @@
 
 echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</title></head><body><ul>'
 for i in `ls dist`; do echo "<li><a href=\"$i\">$i</a></li>"; done
-echo '</ul></body></html>'
+echo '</ul>Last updated on '
+date
+echo '</body></html>'
