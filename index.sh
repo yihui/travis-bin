c6f8ee847e373d6786093ac87ec1617344ffc269
Yihui Xie <xie@yihui.name>
2017-10-24 09:34:59 -0500

    also write out file sizes

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</title></head><body><ul>'
-for i in `ls dist`; do [ ${i} = "index.html" ] || echo "<li><a href=\"$i\">$i</a></li>"; done
+for i in `ls dist`; do [ ${i} = "index.html" ] || echo "<li><a href=\"$i\">$i</a> (`ls -lh dist/$i | tr -s ' ' | cut -d' ' -f5`)</li>"; done
 echo '</ul>Last updated on '
 date
 echo '</body></html>'
