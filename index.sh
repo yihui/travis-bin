9382254a368624abab92b0472984aa69b490b038
Yihui Xie <xie@yihui.name>
2017-10-24 09:18:20 -0500

    do not write the item index.html to index.html

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</title></head><body><ul>'
-for i in `ls dist`; do echo "<li><a href=\"$i\">$i</a></li>"; done
+for i in `ls dist`; do [ ${i} = "index.html" ] || echo "<li><a href=\"$i\">$i</a></li>"; done
 echo '</ul>Last updated on '
 date
 echo '</body></html>'
