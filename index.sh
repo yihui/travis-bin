538a8782153a24072e04d0ba82f164db1210738c
Yihui Xie <xie@yihui.name>
2019-12-10 11:48:52 -0600

    more info on the homepage

--- index.sh
+++ index.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</title></head><body><ul>'
+echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</title></head><body><p>Available binaries:</p><ul>'
 for i in `ls dist`; do [ ${i} = "index.html" ] || echo "<li><a href=\"$i\">$i</a> (`ls -lh dist/$i | tr -s ' ' | cut -d' ' -f5`)</li>"; done
-echo '</ul>Last updated on '
+echo '</ul><pre>'
+pandoc --version
+echo
+pandoc-citeproc --version
+echo
+tlmgr --version
+echo '</pre><p>Last updated on '
 date
-echo '</body></html>'
+echo '</p></body></html>'
