13400e7cf7c86829f79a2c63b9d184d46c3f820a
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-11-13 15:31:02 +0100

    generate the index.html

--- .github/scripts/index.sh
+++ .github/scripts/index.sh
@@ -4,9 +4,9 @@ echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</ti
 echo '<body><p>Available binaries (<a href="https://github.com/yihui/travis-bin">Github repo</a>):</p><ul>'
 for i in `ls dist`; do [ ${i} = "index.html" ] || echo "<li><a href=\"$i\">$i</a> (`ls -lh dist/$i | tr -s ' ' | cut -d' ' -f5`)</li>"; done
 echo '</ul><pre>'
-pandoc --version
+cat dist/pandoc-version.txt
 echo
-tlmgr --version
+cat dist/tinytex-version.txt
 echo '</pre><p>Last updated on '
 date
 echo '</p></body></html>'
