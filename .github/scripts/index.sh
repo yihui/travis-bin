1c2563bb624c88bb5980d58af670286b18333b41
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-11-13 15:34:36 +0100

    hidden version text file

--- .github/scripts/index.sh
+++ .github/scripts/index.sh
@@ -4,9 +4,9 @@ echo '<html><head><title>Pre-built binaries of Pandoc and TeXLive for Travis</ti
 echo '<body><p>Available binaries (<a href="https://github.com/yihui/travis-bin">Github repo</a>):</p><ul>'
 for i in `ls dist`; do [ ${i} = "index.html" ] || echo "<li><a href=\"$i\">$i</a> (`ls -lh dist/$i | tr -s ' ' | cut -d' ' -f5`)</li>"; done
 echo '</ul><pre>'
-cat dist/pandoc-version.txt
+cat dist/.pandoc-version.txt
 echo
-cat dist/tinytex-version.txt
+cat dist/.tinytex-version.txt
 echo '</pre><p>Last updated on '
 date
 echo '</p></body></html>'
